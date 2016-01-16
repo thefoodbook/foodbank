9648b9b693b8c867b0936674751af355a5c7e094
Jun Ong <onejun.ong@gmail.com>
2016-01-16 15:04:49 -0800

    first commit

--- foodbook/urls.py
+++ foodbook/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 ]
+
+
+from django.conf.urls import url
