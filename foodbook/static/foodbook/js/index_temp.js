e246d204a837940a376c707514da80679e0d9993
Tanner <tannerphan@gmail.com>
2016-01-17 15:50:15 -0800

    Add left swipe

--- foodbook/static/foodbook/js/index_temp.js
+++ foodbook/static/foodbook/js/index_temp.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-    $(".buddy").on("swiperight", right);  
-   $(".buddy").on("swipeleft", left);
+  var counter = 0;
+  $(".buddy").on("swiperight", right);  
+  $(".buddy").on("swipeleft", left);
    
-    $(".buddy").on("click", right);
+    /*$(".buddy").on("click", function(){
+      var arr = [1,2,3,4,5,6]
+      console.log(arr[1]);
+      right();
+    });*/
+  $(".buddy").on("click", right)
   
   function right() {
-      $(this).addClass('rotate-left').delay(700).fadeOut(1);
-      $('.buddy').find('.status').remove();
+    counter++;
+      if(counter == 3 || counter ==5 ||counter ==8){ 
+        return left.apply(this);
+      }
 
+      $('.buddy').addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+      console.log(this);
       $(this).append('<div class="status like">Like!</div>');      
       if ( $(this).is(':last-child') ) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
@@ -17,7 +28,7 @@ $(document).ready(function(){
   }
   
   function left() {
-    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddy').addClass('rotate-right').delay(700).fadeOut(1);
     $('.buddy').find('.status').remove();
     $(this).append('<div class="status dislike">Dislike!</div>');
 
@@ -28,5 +39,6 @@ $(document).ready(function(){
         $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
     } 
   }
+
 
 });
