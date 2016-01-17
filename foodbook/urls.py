2097244bc558e6a848609e9b04edd4c716815528
Tanner <tannerphan@gmail.com>
2016-01-16 16:37:10 -0800

    Tanner file structures changed

--- foodbook/urls.py
+++ foodbook/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
--- foodbook/views.py
+++ foodbook/views.py
@@ -4,11 +4,9 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('foodbook/index.html')
-    context = {
-        'latest_question_list': latest_question_list}
-    return render(request, 'foodbook/index.html', context)
+	context = ''
+	return render(request, 'foodbook/index.html')
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
